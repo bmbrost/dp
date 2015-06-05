320419381827f8ce0423c4a5173c0462ebda118a
bmbrost <bmbrost@gmail.com>
2015-06-04 19:15:56 -0600

    Blocked Gibbs sampler works, added update for a0 (concentration parameter)

--- dpmixture.blocked.mcmc.R
+++ dpmixture.blocked.mcmc.R
@@ -1,4 +1,4 @@
-dpmixture.neal.2000.algm.8.mcmc <- function(y,P0,priors,tune,start,n.mcmc){
+dpmixture.blocked.mcmc <- function(y,P0,priors,tune,start,n.mcmc){
   
   library(MCMCpack)  # for Dirichlet distribution functions
   
@@ -6,14 +6,15 @@ dpmixture.neal.2000.algm.8.mcmc <- function(y,P0,priors,tune,start,n.mcmc){
   # browser()
   a0 <- start$a0
   z <- start$z
-  m <- priors$m
+  sigma <- 1
+  N <- priors$N
   n <- length(y)  # number of observations
-  P0.tmp <- P0
   P0 <- range(y)+c(-10,10)  # redefine P0 to contain support of y plus extra
   F.P0 <- 1/(max(P0)-min(P0))
 
   
   z.save <- matrix(0,n,n.mcmc)
+  a0.save <- numeric(n.mcmc)
   keep <- list(z=0)
   
   
@@ -22,42 +23,53 @@ dpmixture.neal.2000.algm.8.mcmc <- function(y,P0,priors,tune,start,n.mcmc){
   ###
   
   for (k in 1:n.mcmc) {
-    
-    ###
-    ### Sample z (cluster assignments for observations)
-    ###
-    
-    # Following Neal (2000), Algorithm 8
 
+    # Following Ishwaran and James (2001); Also Gelman et al. (2014), Section 23.3
 # browser()  
     
-    phi.c <- sort(unique(z))
+    ###
+    ### Sample theta (cluster parameter)
+    ###
+    
+    A <- table(z)/sigma
+    b <- tapply(y,z,sum)/sigma
+    n.theta <- length(A)
+    theta <- c(rnorm(n.theta,b/A,1/A),runif(N-n.theta,min(P0),max(P0)))
+
+    ###
+    ### Sample z (cluster assignments)
+    ###
+    
     for(i in 1:n){
-      delta.j <- table(z[-i])
-      phi.c.tmp <- sort(unique(z[-i]))
-      singleton <- length(phi.c)!=length(phi.c.tmp)
-# see duplicated() in data.table package    
-      if(singleton){
-        h <- c(z[i],runif(m-1,min(P0),max(P0)))
-        p.old <- (delta.j/(n-1+a0))*dnorm(y[i],phi.c.tmp,1)
-        p.new <- ((a0/m)/(n-1+a0))*dnorm(y[i],h,1)
-        z[i] <- sample(c(phi.c.tmp,h),1,prob=c(p.old,p.new))  
-      } 
-      else{
-        h <- runif(m,min(P0),max(P0))    
-        p.old <- (delta.j/(n-1+a0))*dnorm(y[i],phi.c.tmp,1)
-        p.new <- ((a0/m)/(n-1+a0))*dnorm(y[i],h,1)
-        z[i] <- sample(c(phi.c.tmp,h),1,prob=c(p.old,p.new))  
-      }
+      p.tmp <- pie*dnorm(y[i],theta,sigma)
+      idx <- sample(1:N,1,prob=p.tmp/sum(p.tmp))
+      z[i] <- theta[idx]
     }
     
-      
+    
+    ###
+    ### Sample p (stick-breaking process)
+    ###
+
+    z.tab <- table(z)
+    n.theta <- length(z.tab)
+    z.tab <- c(z.tab,rep(0,N-n.theta))
+    v <- c(rbeta(N-1,1+z.tab[-N],a0+n-cumsum(z.tab[-N])),1)
+    pie <- v*c(1,cumprod((1-v[-N])))  
+
+    ###
+    ### Sample a0 (concentration parameter); See Gelman section 23.3
+    ###
+
+    a0 <- rgamma(1,priors$r+N-1,priors$q-sum(log(1-v[-N])))  
+    
+    
     ###
     ###  Save samples 
     ###
     
     z.save[,k] <- z
-    
+    a0.save[k] <- a0    
   }
   
   ###
@@ -66,6 +78,6 @@ dpmixture.neal.2000.algm.8.mcmc <- function(y,P0,priors,tune,start,n.mcmc){
   
   keep$z <- keep$z/(n.mcmc*n)
   cat(paste("\nz acceptance rate:",keep$z)) 
-  list(z=z.save,n.mcmc=n.mcmc)
+  list(z=z.save,a0=a0.save,n.mcmc=n.mcmc)
  
 }
