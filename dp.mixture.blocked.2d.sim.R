f867d7cab922e68a4ad2d190dc6d7f877bb9a42d
bmbrost <bmbrost@gmail.com>
2015-06-16 12:24:29 -0600

    Very minor changes

--- dp.mixture.blocked.2d.sim.R
+++ dp.mixture.blocked.2d.sim.R
@@ -17,6 +17,7 @@ P0 <- cbind(c(-2,2,2,-2,-2),c(-2,-2,2,2,-2))  # 2-D uniform base probability mea
 H <- 50  # maximum number of clusters for truncation approximation
 sim1 <- stick.2d(n,P0,a0,H)
 z <- sim1$z  # cluster assignments
+z.tab <- table(z)
 
 # Generate observations conditional on cluster assignments
 sigma <- 0.1
@@ -38,12 +39,13 @@ source("/Users/brost/Documents/git/DPMixtures/dp.mixture.blocked.2d.mcmc.R")
 # hist(rgamma(1000,2,2),breaks=100)
 # hist(rgamma(1000,1,1),breaks=100)
 start <- list(a0=a0,z=z,#z=fitted(kmeans(y,rpois(1,10))),
-  sigma=sigma,pie=rdirichlet(1,rep(1/H,H)))
+  sigma=sigma,pie=rdirichlet(1,rep(1/H,H))) #sim1$pie)  # 
 out1 <- dpmixture.blocked.2d.mcmc(y,P0,
   priors=list(H=H,r=20,q=10,sigma.l=0,sigma.u=5),
-  tune=list(z=0.5,sigma=0.1),start=start,n.mcmc=100)
+  tune=list(z=0.5,sigma=0.01),start=start,n.mcmc=1000)
 
 mod <- out1
+idx <- 1:100
 idx <- 1:1000
 idx <- 1:5000
 idx <- 9000:10000
@@ -52,10 +54,12 @@ idx <- 9000:10000
 b <- 3*c(-sigma,sigma) # Plot buffer for errors
 plot(0,0,xlim=range(P0[,1])+b,ylim=range(P0[,2])+b,pch="",yaxt="n",xaxt="n",xlab="",ylab="")
 polygon(x=P0[,1],y=P0[,2],col="gray85")
-points(z,pch=19,cex=1,col=rgb(1,0,0,0.15))
-points(mod$z[,1,],mod$z[,2,],pch=19,cex=0.1)
+points(apply(mod$z,2,I),pch=19,cex=0.5,col=rgb(0,0,0,0.15))
 points(y,pch=19,cex=0.5,col=3)
+points(z,pch=19,cex=1,col=rgb(1,0,0,1))
 
+cl <- kmeans(apply(mod$z,2,I),11)
+points(cl$centers,col=4,pch=19)
 
 
 # Concentration parameter
@@ -66,8 +70,10 @@ mean(mod$a0[idx])*log(n)
 hist(mod$sigma[idx],breaks=100);abline(v=sigma,col=2,lty=2)
 
 # Modeled number of clusters
-plot(apply(mod$z[,idx],2,function(x) length(unique(x))),type="l")  
-abline(h=length(z.tab),col=2,lty=2)  # true number of clusters
+plot(apply(mod$z[,,idx],c(3),function(x) nrow(unique(x))),type="l")  
+abline(h=nrow(unique(z)),col=2,lty=2)  # true number of clusters
+
+
 
 
 plot(apply(mod$z[,idx],2,max),type="l")
