b76322de7bb982d0c55c2685168130b32512a23e
bmbrost <bmbrost@gmail.com>
2015-11-01 10:16:41 -0700

    Revised dp.mixture.blocked.2d to resemble implementation of haulouts.3; renamed to dp.mixture.2d

---
+++ dp.mixture.2d.mcmc.R
@@ -0,0 +1,314 @@
+# Dirichlety process mixture model for 2-dimensional data
+
+dpmixture.2d.mcmc <- function(s,S.tilde,priors,tune,start,n.mcmc,n.cores=NULL){
+  
+	t.start <- Sys.time()
+  
+	###
+	### Libraries and Subroutines
+	###
+  
+	# library(MCMCpack)  # for Dirichlet distribution functions
+	# library(data.table)  # for tabulating and summing
+	# library(dplyr)  # dense_rank() for ranking clusters smallest to largest
+	# library(doParallel) #For parallel processing
+	# library(foreach) #For parallel processing  
+  
+  	get.mu.0 <- function(x,s,h,sigma){
+		# browser()
+		idx <- which(h==x)
+		n <- length(idx)
+		Sigma.inv <- solve(sigma^2*diag(2))
+		b <- colSums(s[idx,]%*%Sigma.inv)
+		A.inv <- solve(n*Sigma.inv)
+		rnorm(2,A.inv%*%b,sqrt(diag(A.inv)))	# proposal for mu.0	
+	}
+
+	###
+	### Starting values and priors
+	###
+  
+	theta <- start$theta
+	mu.0 <- start$mu.0
+	sigma <- start$sigma
+	pie <- start$pie
+	H <- priors$H
+ 
+  
+	###
+	###  Setup Variables 
+	###
+  
+    # browser()  
+	T <- nrow(s)  # number of observations
+
+	# Setup up Dirichlet process mixture variables
+	mu.0 <- unique(mu.0) #  unique clusters
+	m <- nrow(mu.0)  # number of clusters
+	ht <- c(1:m)[match(start$mu.0[,1],mu.0[,1])]  # idx assigning each obs to record in mu.0
+	tab <- table(ht)  # tabulate cluster membership
+	ord <- order(tab,decreasing=TRUE)  # clusters ordered by membership
+	samp <- as.numeric(names(tab))[ord]
+
+
+	###
+	### Create receptacles for output
+	###
+
+	mu.0.save <- array(0,dim=c(T,2,n.mcmc))
+	theta.save <- numeric(n.mcmc)
+	sigma.save <- numeric(n.mcmc)  
+	m.save <- numeric(n.mcmc)  
+	keep <- list(sigma=0)
+
+    
+	###
+	### Begin MCMC loop
+	###
+  
+	for (k in 1:n.mcmc) {
+    	if(k%%1000==0) cat(k,"");flush.console()
+# print(k)    
+		###
+		### Dirichlet process parameters
+		###	
+
+		# Update follows the blocked Gibbs sampler of Ishwaran and James (2001)
+		# and Gelman et al. (2014), Section 23.3
+	
+		# Sample 'occupied' mu.0 (true location of clusters with non-zero membership)		 
+	    # Sampler currently disregards support P0
+# browser()
+		mu.0 <- t(sapply(samp,function(x) get.mu.0(x,s,ht,sigma)))
+		
+		# Sample 'unoccupied' mu.0 (clusters with zero membership) from prior 
+	    mu.0 <- rbind(mu.0,cbind(runif(H-m,min(S.tilde[,1]),max(S.tilde[,1])),
+			runif(H-m,min(S.tilde[,2]),max(S.tilde[,2]))))
+
+	    # Note: sampling order matters for the remaining DP updates. Cluster parameters 
+	    # must be sampled in same order as pie, i.e., sorted by decreasing membership
+    
+	    # Sample cluster assignment indicator
+		samp <- c(samp,c(1:H)[-samp])
+		ht <- sapply(1:T,function(x) sample(samp,1,
+			prob=exp(log(pie)+dnorm(s[x,1],mu.0[samp,1],sigma,log=TRUE)+
+			dnorm(s[x,2],mu.0[samp,2],sigma,log=TRUE))))
+		mu.0.tmp <- mu.0[ht,]
+
+		# Tabulate cluster membership with base functions
+		tab <- table(ht) #  tabulate cluster membership
+		m <- length(tab) #  number of clusters
+		ord <- order(tab,decreasing=TRUE) #  idx of tab ordered by membership
+		samp <- as.numeric(names(tab))[ord]
+		
+		# Stick-breaking process
+		pad <- ifelse(m<H,H-m-1,0)
+		tab.tmp <- c(tab[ord],rep(0,pad))  # membership in decreasing order
+		eta <- c(rbeta(H-1,1+tab.tmp,theta+T-cumsum(tab.tmp)),1)  # stick-breaking weights
+	    pie <- eta*c(1,cumprod((1-eta[-H])))  # mixture component probabilities
+   
+		# Sample theta (concentration parameter)
+	    # See Gelman section 23.3, Ishwaran and Zarepour (2000)
+       	
+       	theta <- rgamma(1,priors$r+H-1,priors$q-sum(log(1-eta[-H])))  
+
+		
+		###
+		### Sample sigma (observation error)
+	    ###
+
+    	sigma.star <- rnorm(1,sigma,tune$sigma)
+	    if(sigma.star>priors$sigma.l & sigma.star<priors$sigma.u){
+			mh.star.sigma <- sum(dnorm(s[,1],mu.0.tmp[,1],sigma.star,log=TRUE)+
+	        	dnorm(s[,2],mu.0.tmp[,2],sigma.star,log=TRUE))
+    		mh.0.sigma <- sum(dnorm(s[,1],mu.0.tmp[,1],sigma,log=TRUE)+
+		        dnorm(s[,2],mu.0.tmp[,2],sigma,log=TRUE))
+		    if(exp(mh.star.sigma-mh.0.sigma)>runif(1)){
+        		sigma <- sigma.star
+		        keep$sigma <- keep$sigma+1
+    		} 
+	    }
+ 
+
+	    ###
+    	###  Save samples 
+	    ###
+
+    	mu.0.save[,,k] <- mu.0.tmp
+		m.save[k] <- m
+    	theta.save[k] <- theta    
+	    sigma.save[k] <- sigma
+	}
+  
+	###
+	### Write output
+	###
+  
+	keep$sigma <- keep$sigma/n.mcmc
+	cat(paste("\nsigma acceptance rate:",keep$sigma)) 
+	cat(paste("\nTotal time elapsed:",round(difftime(Sys.time(),t.start,units="mins"),2)))
+	list(mu.0=mu.0.save,theta=theta.save,sigma=sigma.save,m=m.save,keep=keep,n.mcmc=n.mcmc)
+ }
+
+
+
+### This version uses data.table for tabluation. Reference simulation code that's 
+### commented out at the bottom of dp.mixture.blocked.2d.sim.R
+
+# dpmixture.blocked.2d.mcmc <- function(y,P0,priors,tune,start,n.mcmc,n.cores=NULL){
+  
+  # t.start <- Sys.time()
+  
+  # ###
+  # ### Libraries and Subroutines
+  # ###
+  
+  # library(MCMCpack)  # for Dirichlet distribution functions
+  # library(data.table)  # for tabulating and summing
+  # library(dplyr)  # dense_rank() for ranking clusters smallest to largest
+  # library(doParallel) #For parallel processing
+  # library(foreach) #For parallel processing  
+  
+  # get.mu.0 <- function(x,dt,tab,sigma){
+    # y.tmp <- dt[z1==tab[x,z1]&z2==tab[x,z2],.(y1,y2)]
+    # y.tmp <- as.matrix(y.tmp)
+    # A <- solve(sigma^2*diag(2))*tab[x,N]
+	# A.inv <- solve(A)
+    # b <- colSums(y.tmp%*%solve(sigma^2*diag(2)))
+    # t(A.inv%*%b)
+    # rnorm(2,A.inv%*%b,sqrt(diag(A.inv)))
+  # }
+  
+  
+  # ###
+  # ###  Create cluster for parallel processing
+  # ###
+  
+  # #   if(is.null(n.cores)) n.cores <- detectCores() - 1
+  # #   if(n.cores==1) registerDoSEQ() else registerDoParallel(cores=n.cores) # multicore functionality	
+  # #   mcoptions <- list(preschedule=TRUE)
+  # #   cat(paste("\nUsing",n.cores,"cores for parallel processing."))
+  
+  
+  # ###
+  # ### Starting values and priors
+  # ###
+  
+  # a0 <- start$a0
+  # z <- start$z
+  # sigma <- start$sigma
+  # pie <- start$pie
+  # H <- priors$H
+  
+  
+  # ###
+  # ###  Setup Variables 
+  # ###
+  
+  # #   browser()  
+  # n <- nrow(y)  # number of observations
+  # dt <- as.data.table(cbind(y1=y[,1],y2=y[,2],z1=z[,1],z2=z[,2]))
+  # tab <- dt[,.N,by=.(z1,z2)]  
+  # setkey(tab ,N)
+  # n.cluster <- tab[,.N]  
+  # ord <- n.cluster:1
+  
+  # z.save <- array(0,dim=c(n,2,n.mcmc))
+  # a0.save <- numeric(n.mcmc)
+  # sigma.save <- numeric(n.mcmc)  
+  # keep <- list(sigma=0)
+
+    
+  # ###
+  # ### Begin MCMC loop
+  # ###
+  
+  # for (k in 1:n.mcmc) {
+    # if(k%%1000==0) cat(k,"");flush.console()
+    
+    # # Following Ishwaran and James (2001); Also Gelman et al. (2014), Section 23.3
+    
+    # ###
+    # ### Sample theta (cluster parameter)
+    # ###
+    
+    # # Sampler currently disregards support P0
+# # browser()
+    # mu.0 <- t(sapply(1:n.cluster,function(x) get.mu.0(x,dt,tab,sigma)))
+    # # mu.0 <- cbind(rnorm(n.cluster,mu.0[ord,1],sigma/sqrt(tab[ord,N])),
+      # # rnorm(n.cluster,mu.0[ord,2],sigma/sqrt(tab[ord,N])))  
+    # n.cluster.new <- H-n.cluster
+    # mu.0 <- rbind(mu.0[ord,],cbind(runif(n.cluster.new,P0[1,1],P0[2,1]),
+      # runif(n.cluster.new,P0[1,2],P0[3,2])))
+       
+  
+    # ###
+    # ### Sample h.t (cluster assignments)
+    # ###
+    
+    # # browser()
+    # h.t <- sapply(1:n,function(x) sample(1:H,1,
+      # prob=pie*dnorm(y[x,1],mu.0[,1],sigma)*dnorm(y[x,2],mu.0[,2],sigma)))
+    # dt[,z1:=mu.0[h.t,1]]
+    # dt[,z2:=mu.0[h.t,2]]    
+
+
+    # ###
+    # ### Sample sigma (observation error)
+    # ###
+
+    # sigma.star <- rnorm(1,sigma,tune$sigma)
+    # if(sigma.star>priors$sigma.l & sigma.star<priors$sigma.u){
+      # mh.star.sigma <- sum(dnorm(dt[,y1],dt[,z1],sigma.star,log=TRUE)+
+        # dnorm(dt[,y2],dt[,z2],sigma.star,log=TRUE))
+      # mh.0.sigma <- sum(dnorm(dt[,y1],dt[,z1],sigma,log=TRUE)+
+        # dnorm(dt[,y2],dt[,z2],sigma,log=TRUE))
+      # if(exp(mh.star.sigma-mh.0.sigma)>runif(1)){
+        # sigma <- sigma.star
+        # keep$sigma <- keep$sigma+1
+      # } 
+    # }
+
+
+    # ###
+    # ### Sample p (stick-breaking process)
+    # ###
+
+    # tab <- dt[,.N,by=.(z1,z2)]  
+    # setkey(tab ,N)
+    # n.cluster <- tab[,.N]  
+    # ord <- n.cluster:1
+    # tab.tmp <- c(tab[ord,N],rep(0,H-n.cluster-1))
+
+    # # Update stick-breaking weights
+    # v <- c(rbeta(H-1,1+tab.tmp,a0+n-cumsum(tab.tmp)),1)
+    # pie <- v*c(1,cumprod((1-v[-H])))  # mixture component probabilities
+
+    
+    # ###
+    # ### Sample a0 (concentration parameter); See Gelman section 23.3
+    # ###
+    
+    # a0 <- rgamma(1,priors$r+H-1,priors$q-sum(log(1-v[-H])))  
+    
+
+    # ###
+    # ###  Save samples 
+    # ###
+
+    # z.save[,1,k] <- dt[,z1]
+    # z.save[,2,k] <- dt[,z2]
+    # a0.save[k] <- a0    
+    # sigma.save[k] <- sigma
+  # }
+  
+  # ###
+  # ### Write output
+  # ###
+  
+  # keep$sigma <- keep$sigma/n.mcmc
+  # cat(paste("\nsigma acceptance rate:",keep$sigma)) 
+  # cat(paste("\nTotal time elapsed:",round(difftime(Sys.time(),t.start,units="mins"),2)))
+  # list(z=z.save,a0=a0.save,sigma=sigma.save,keep=keep,n.mcmc=n.mcmc)
+  
+# }
---
+++ dp.mixture.2d.sim.R
@@ -0,0 +1,211 @@
+# Simulate 2-dimensional data for a Dirichlet process mixture.
+# Use dp.mixture.2d.mcmc.R for model fitting.
+
+rm(list=ls())
+
+library(cluster)
+# library(msm)
+# library(pscl)
+library(MCMCpack)  # for rdirichlet(...)
+library(DPpackage)
+
+# source("/Users/brost/Documents/git/DPMixtures/dp.utils.R")  # sim functions
+
+#############################################################################
+### Define support of cluster centroids
+#############################################################################
+
+S.tilde <- cbind(c(-2,2,2,-2,-2),c(-2,-2,2,2,-2))  # 2D uniform base probability measure
+
+
+#############################################################################
+### Simulate clusters and assignments using a stick-breaking process
+### See Ishwaran and James (2001), Gelman et al. (2014), Section 23.2
+#############################################################################
+
+T <- 500  # number of observations to simulate
+theta <- 1.5  # Dirichlet process mixture concentration parameter
+H <- 50  # maximum number of clusters for truncation approximation
+
+# Prior elicitation for theta
+E.m <- theta*(digamma(theta+T)-digamma(theta))  # expected number of clusters
+theta.priors <- DPelicit(T,mean=E.m,std=3,method="JGL")$inp  # Gamma(r,q) prior for theta
+
+# Stick-breaking process
+eta <- c(rbeta(H-1,1,theta),1)  # stick-breaking weights
+pie <- eta*c(1,cumprod((1-eta[-H])))  # probability mass
+plot(pie,type="b")
+
+# Simulate possible clusters from S.tilde
+mu.0.tmp <- cbind(runif(H,min(S.tilde[,1]),max(S.tilde[,1])),
+	runif(H,min(S.tilde[,2]),max(S.tilde[,2])))
+plot(mu.0.tmp) #  possible cluster locations
+
+# Simulate realized clusters locations
+ht <- sample(1:H,T,replace=TRUE,prob=pie)  # latent cluster assignments
+m <- length(unique(ht))  # number of clusters
+tab <- table(ht)  # tabulate cluster membership
+mu.0 <- mu.0.tmp[ht,] #  occupied clusters
+
+# Plot realized cluster locations
+points(mu.0,pch=19,cex=0.5,col=2)
+
+
+#############################################################################
+### Simulate observations conditional on cluster assignments
+#############################################################################
+
+sigma <- 0.25
+s <- matrix(rnorm(T*2,mu.0,sigma),,2)
+
+b <- 3*c(-sigma,sigma) # Plot buffer for errors
+plot(0,0,xlim=range(S.tilde[,1])+b,ylim=range(S.tilde[,2])+b,pch="",yaxt="n",xaxt="n",xlab="",ylab="")
+polygon(x=S.tilde[,1],y=S.tilde[,2],col="gray85")
+points(s,pch=19,cex=0.5,col=rgb(0,0,0,0.25))
+points(mu.0,pch=19,cex=0.75,col=2)
+
+
+#############################################################################
+### Fit models
+#############################################################################
+
+start <- list(theta=theta,mu.0=mu.0,pie=pie,sigma=sigma)
+priors <- list(H=H,r=theta.priors[1],q=theta.priors[2],sigma.l=0,sigma.u=5)
+tune <- list(sigma=0.025)
+source("/Users/brost/Documents/git/DPMixtures/dp.mixture.2d.mcmc.R")
+out1 <- dpmixture.2d.mcmc(s,S.tilde,priors=priors,tune=tune,start=start,n.mcmc=1000)
+
+mod <- out1
+idx <- 1:1000
+
+# True clusters
+b <- 3*c(-sigma,sigma) # Plot buffer for errors
+plot(0,0,xlim=range(S.tilde[,1])+b,ylim=range(S.tilde[,2])+b,pch="",yaxt="n",xaxt="n",xlab="",ylab="")
+polygon(x=S.tilde[,1],y=S.tilde[,2],col="gray85")
+points(s,pch=19,cex=0.5,col=rgb(0,0,0,0.25))
+points(mod$mu.0[,1,idx],mod$mu.0[,2,idx],cex=0.01,col=3)
+points(mu.0,pch=19,cex=0.75,col=2)
+
+# Inference on theta: DP concentration parameter
+hist(mod$theta[idx],breaks=100);abline(v=theta,col=2,lty=2) 
+mean(mod$theta[idx])*log(T)
+
+# Inference on m: modeled number of clusters
+plot(mod$m,type="l");abline(h=m,col=2,lty=2)  # true number of clusters  
+barplot(table(mod$m)) 
+
+# Observation error
+hist(mod$sigma[idx],breaks=100);abline(v=sigma,col=2,lty=2)
+
+# Examine individual observation
+pt.idx <- 2
+plot(mod$mu.0[pt.idx,1,idx],mod$mu.0[pt.idx,2,idx],pch=19,col=rgb(0,0,0,0.25),cex=0.25,
+	ylim=range(c(mod$mu.0[pt.idx,2,idx],s[pt.idx,2])),
+	xlim=range(c(mod$mu.0[pt.idx,1,idx],s[pt.idx,1])),asp=1)
+points(s[pt.idx,1],s[pt.idx,2],col=2,pch=19)
+points(s,col=3,cex=0.5,pch=19)
+identify(s)
+
+
+
+
+
+
+# rm(list=ls())
+
+# library(cluster)
+# # library(msm)
+# # library(pscl)
+# library(MCMCpack)  # for rdirichlet(...)
+
+# source("/Users/brost/Documents/git/DPMixtures/dp.utils.R")  # sim functions
+
+# ###
+# ### Simulation 2-dimensional Dirichlet process mixture
+# ###
+
+# # Using stick-breaking process (Gelman et al. 2014, BDA Section 23.2)
+# n <- 500  # number of observations to simulate
+# a0 <- 1.5  # concentration parameter
+# P0 <- cbind(c(-2,2,2,-2,-2),c(-2,-2,2,2,-2))  # 2-D uniform base probability measure
+# H <- 50  # maximum number of clusters for truncation approximation
+# sim1 <- stick.2d(n,P0,a0,H)
+# z <- sim1$z  # cluster assignments
+# z.tab <- table(z)
+
+# # Generate observations conditional on cluster assignments
+# sigma <- 0.1
+# y <- matrix(rnorm(n*2,z,sigma),,2)
+
+# b <- 3*c(-sigma,sigma) # Plot buffer for errors
+# plot(0,0,xlim=range(P0[,1])+b,ylim=range(P0[,2])+b,pch="",yaxt="n",xaxt="n",xlab="",ylab="")
+# polygon(x=P0[,1],y=P0[,2],col="gray85")
+# points(y,pch=19,cex=0.5)
+# points(z,pch=19,cex=1,col=rgb(1,0,0,0.15))
+
+
+# ###
+# ### Fit models
+# ###
+
+# # Fit model using blocked Gibbs sampler 
+# source("/Users/brost/Documents/git/DPMixtures/dp.mixture.blocked.2d.mcmc.R")
+# # hist(rgamma(1000,2,2),breaks=100)
+# # hist(rgamma(1000,1,1),breaks=100)
+# start <- list(a0=a0,z=z,#z=fitted(kmeans(y,rpois(1,10))),
+  # sigma=sigma,pie=rdirichlet(1,rep(1/H,H))) #sim1$pie)  # 
+# start <- list(a0=a0,z=z,#z=fitted(kmeans(y,rpois(1,10))),
+  # sigma=sigma,pie=sim1$pie)  # 
+# out1 <- dpmixture.blocked.2d.mcmc(y,P0,
+  # priors=list(H=H,r=2,q=2,sigma.l=0,sigma.u=5),
+  # tune=list(z=0.5,sigma=0.01),start=start,n.mcmc=2000)
+
+# mod <- out1
+# idx <- 1:100
+# idx <- 1:2000
+# idx <- 1000:2000
+# idx <- 1:2500
+# idx <- 9000:10000
+
+# # True clusters
+# b <- 3*c(-sigma,sigma) # Plot buffer for errors
+# plot(0,0,xlim=range(P0[,1])+b,ylim=range(P0[,2])+b,pch="",yaxt="n",xaxt="n",xlab="",ylab="")
+# polygon(x=P0[,1],y=P0[,2],col="gray85")
+# points(mod$z[,1,idx],mod$z[,2,idx],pch=19,cex=0.5,col=rgb(0,0,0,0.0025))
+# points(y,pch=19,cex=0.25,col=3)
+# points(z,pch=19,cex=0.5,col=rgb(1,0,0,1))
+
+# cl <- kmeans(apply(mod$z,2,I),11)
+# points(cl$centers,col=4,pch=19)
+
+# # Concentration parameter
+# hist(mod$a0[idx],breaks=100,col="gray90");abline(v=a0,col=2,lty=2) 
+# mean(mod$a0[idx])*log(n)
+
+# # Observation error
+# hist(mod$sigma[idx],breaks=100);abline(v=sigma,col=2,lty=2)
+
+# # Modeled number of clusters
+# nclust <- apply(mod$z[,,idx],c(3),function(x) nrow(unique(x)))
+# plot(nclust,type="l")
+# abline(h=nrow(unique(z)),col=2,lty=2)  # true number of clusters  
+# barplot(table(nclust))
+
+
+
+# plot(apply(mod$z[,idx],2,max),type="l")
+# cl.ranks <- apply(mod$z[,idx],2,dense_rank)
+# plot(c(mod$z[,idx])[c(cl.ranks)==8],type="l")
+
+# hist(c(mod$z[,idx])[c(test)==1],breaks=500,xlim=range(mod$z[idx]),ylim=c(0,5),prob=TRUE)  
+# hist(c(mod$z[,idx])[c(test)==5],col=5,breaks=500,add=TRUE,prob=TRUE)  
+
+# pt.idx <- 94
+# plot(mod$z[pt.idx,idx],type="l");abline(h=z[pt.idx],col="red",lty=2)
+# hist(mod$z[,idx],breaks=5000,xlim=c(range(mod$z[pt.idx,])+c(-10,10)),prob=TRUE)
+# hist(mod$z[pt.idx,idx],breaks=50,col="red",add=TRUE,border="red",prob=TRUE);abline(v=z[pt.idx],col="red",lty=2)
+# points(y[pt.idx],-0.010,pch=19)
+
+# which.max(apply(mod$z,1,var))
+# abline(v=29.75)
+# which.min(sapply(y,function(x) dist(c(x,29.75))))
