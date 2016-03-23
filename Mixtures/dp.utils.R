c9b876919198b7ba78c6efe5372d626fd3abef95
bmbrost <bmbrost@gmail.com>
2016-03-22 23:21:01 -0600

    Revert "Delete dp.utils.R"
    This reverts commit 59a715c0459942c234412897af7981253097df8d.

---
+++ Mixtures/dp.utils.R
@@ -0,0 +1,68 @@
+stick <- function(n,P0,a0,H){
+  
+  # Simulate Dirichlet process data using stick-breaking construction
+  # See Gelman et al. (2014) BDA, Section 23.2
+  
+  # Arguments: n = number of observations to simulate,
+  # P0 = upper and lower limits of uniform base probability measure,
+  # a0 = concentration parameter, H = maximum number of clusters
+  # for truncation approximation.
+  
+  v <- c(rbeta(H-1,1,a0),1)
+  pie <- v*c(1,cumprod((1-v[-H])))
+  theta <- runif(H,min(P0),max(P0))  # clusters randomly drawn from P0
+  z <- sample(theta,n,replace=TRUE,prob=pie)  # cluster assignments for observations  
+  list(z=z,theta=theta,pie=pie,v=v,n=n,P0=P0,a0=a0,H=H)  
+}
+
+ 
+crp <- function(n,P0,a0){
+  
+  # Simulate Dirichlet process data using Chinese restaurant process
+    
+  # Arguments: n = number of observations to simulate,
+  # P0 = upper and lower limits of discrete uniform base probability measure,
+  # a0 = concentration parameter
+  
+  library(data.table)
+    
+  z <- numeric(n)
+  z <- as.data.table(z)
+  z[1] <- runif(1,P0[1],P0[2])
+ 
+  for (i in 2:n) {
+    tab <- z[1:(i-1),.N,by="z"]
+    p.old <- tab[,N]/(a0+n-1)
+    p.new <- a0/(a0+n-1)
+    z[i] <- sample(c(tab[,z],runif(1,P0[1],P0[2])),1,prob=c(p.old,p.new))
+  }
+  list(z=z[,z],n=n,P0=P0,a0=a0)  
+}
+
+stick.2d <- function(n,P0,a0,H){
+  
+  # Simulate Dirichlet process data in 2 dimensions 
+  # using stick-breaking construction.
+  # See Gelman et al. (2014) BDA, Section 23.2
+  
+  # Arguments: n = number of observations to simulate,
+  # P0 = support of 2-D uniform base probability measure,
+  # a0 = concentration parameter, H = maximum number of clusters
+  # for truncation approximation.
+  
+  v <- c(rbeta(H-1,1,a0),1)
+  pie <- v*c(1,cumprod((1-v[-H])))
+  theta <- cbind(runif(H,min(P0[,1]),max(P0[,1])),runif(H,min(P0[,2]),max(P0[,2])))  # clusters randomly drawn from P0
+  z <- sample(1:H,n,replace=TRUE,prob=pie)  # cluster assignments for observations  
+  list(z=theta[z,],theta=theta,pie=pie,v=v,n=n,P0=P0,a0=a0,H=H)  
+}
+
+polya <- function(n,P0,a0){
+  
+  # Simulate Dirichlet process data using Polya urn scheme
+  
+  # Arguments: n = number of observations to simulate,
+  # P0 = upper and lower limits of discrete uniform base probability measure,
+  # a0 = concentration parameter
+  
+}
